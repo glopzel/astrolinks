1a6c5d9eb9d2b7ac7db7f10770bfbfcc9de16fd0
glopzel <74939915+lozzel@users.noreply.github.com>
2022-08-27 18:03:37 -0400

    change title

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,5 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
-    site: 'https://glopzel.github.io',
-    base: './astrolinks'
+    integrations: [tailwind()]
 });
